63199ecb2980a3563db889e0501d368b2abd2746
aldo_bedrij <wbedrij@gmail.com>
2015-11-26 10:04:41 -0300

    colores

--- rae.py
+++ rae.py
@@ -8,16 +8,82 @@ from html.parser import HTMLParser
 import os
 import urllib
 
+import pdb
+
 import sys
 
+ESC = '\033['
+colours = { 
+'none' : "", 
+'default' : ESC+"0m", 
+'bold' : ESC+"1m", 
+'underline' : ESC+"4m", 
+'blink' : ESC+"5m", 
+'reverse' : ESC+"7m", 
+'concealed' : ESC+"8m", 
+
+'black' : ESC+"30m", 
+'red' : ESC+"31m", 
+'green' : ESC+"32m", 
+'yellow' : ESC+"33m", 
+'blue' : ESC+"34m", 
+'magenta' : ESC+"35m", 
+'cyan' : ESC+"36m", 
+'white' : ESC+"37m", 
+
+'on_black' : ESC+"40m", 
+'on_red' : ESC+"41m", 
+'on_green' : ESC+"42m", 
+'on_yellow' : ESC+"43m", 
+'on_blue' : ESC+"44m", 
+'on_magenta' : ESC+"45m", 
+'on_cyan' : ESC+"46m", 
+'on_white' : ESC+"47m", 
+
+'beep' : "\007", 
+
+# non-standard attributes, supported by some terminals 
+'dark' : ESC+"2m", 
+'italic' : ESC+"3m", 
+'rapidblink' : ESC+"6m", 
+'strikethrough': ESC+"9m", 
+} 
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def ircify(html):
-    begin_regex = re.compile('^[^\0]*<span class\=\"f\"([^<]+?)?>')
+    #html = html.replace("<B>", colours['bold']).replace("</B>", colours['default'])
+    #html = html.replace("<i>", colours['bold']+colours['italic']).replace("</i>", colours['default'])
+
+    begin_regex = re.compile('^[^\0]*?<span class\=\"f\"([^<]+?)?>')
     span_regex = re.compile('<p class\=\"[pq]\"([^<]+?)?>')
     end_regex = re.compile('<p class\=\"o\"([^<]+?)?>[^\0]*$')
+
+    etimologia = re.compile('<span class\=\"a\"([^<]+?)?>')
+    ejemplos = re.compile('<span class\=\"h\"([^<]+?)?>')
+    enfasis = re.compile('<span class\=\"j\"([^<]+?)?>')
+    frases = re.compile('<span class\=\"k\"([^<]+?)?>')
+    genero = re.compile('<span class\=\"[dg]\"([^<]+?)?>')
+
+    html = html.replace("</span>", colours['default'])
 
     html = begin_regex.sub('', html)
     html = span_regex.sub('\n', html)
     html = end_regex.sub('', html)
+
+    html = etimologia.sub(colours['yellow'], html)
+    html = ejemplos.sub(colours['cyan'], html)
+    html = enfasis.sub(colours['green'], html)
+    html = frases.sub(colours['red'], html)
+    html = genero.sub(colours['magenta'], html)
     return html
 
 class MLStripper(HTMLParser):
@@ -42,7 +108,6 @@ def retrieve(palabra):
     buffer = BytesIO()
     #change to use `requests'
     c = pycurl.Curl()
-    #c.setopt(c.URL, 'http://dle.rae.es/?w='+palabra) #actualizado
     address = 'http://lema.rae.es/drae/srv/search?'
     palabra_enc = urllib.parse.urlencode({'key':palabra})
     c.setopt(c.URL, address+palabra_enc)
@@ -71,18 +136,29 @@ def retrieve(palabra):
     body = buffer.getvalue().decode()
     return body
 
-def main():
-    for K in range(1, len(sys.argv)):
-        palabra = sys.argv[K]
-        print(palabra, end="")
-        body = strip_tags(ircify(retrieve(palabra)))
+def the_main(argv):
+    for K in range(1, len(argv)):
+        palabra = argv[K]
+        body = retrieve(palabra)
+        body = strip_tags(ircify(body))
 
         if body.find('No encontrado') != -1:
+            print(palabra, end="")
             print(".\nNo encontrado")
         else:
             print(body)
 
         print('')
+
+def main():
+    the_main(sys.argv)
+
+def test():
+    with open('response.html', 'r') as Input:
+        data = Input.read()
+        #pdb.set_trace()
+        body = strip_tags(ircify(data))
+        print(body)
 
 if  __name__ =='__main__':
     main()
